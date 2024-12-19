fd968dddc445569751d1ecd2391bc2277fbe83e8
smanasreh6 <smanasreh6@gatech.edu>
2024-12-19 15:22:00 -0500

    initial commit

---
+++ day1.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('data/day1.csv')
+
+arr = df.to_numpy()
+
+col1 = arr[:, 0]
+col2 = arr[:, 1]
+
+sort1 = np.sort(col1, axis = 0)
+sort2 = np.sort(col2, axis = 0)
+
+# part 1 result
+res = np.sum(np.abs(sort2 - sort1))
+print(res)
+
+unique = np.unique(col1)
+
+reshaped = col2[:, np.newaxis]
+print(reshaped)
+
+sumit = np.sum(col2[:, None] == unique, axis=0)
+
+# part 2 result
+res2 = np.sum(sumit * unique)
+print(res2)
+
